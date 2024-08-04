e1b16ac635e60946242a61f5bdd5778591948f7b
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-04 09:21:51 +0900

    DB接続処理追加

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/posts");
 const PORT = 3000;
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_CONN_STR)
 
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
