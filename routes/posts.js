bd913e752620c1ea772479ac8d1dafdbf6e3123f
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-06 22:24:08 +0900

    投稿削除追加

--- routes/posts.js
+++ routes/posts.js
@@ -29,4 +29,19 @@ router.put("/:id", async (req, res) => {
     }
 });
 
+// 投稿削除
+router.delete("/:id/delete", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne();
+            return res.status(200).json("投稿削除が成功しました");
+        } else {
+            return res.status(403).json("他の人の投稿を削除できません");
+        }
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
 module.exports = router;
