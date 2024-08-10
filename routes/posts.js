cbcf302f8db8c1018b642af2182a08cf0856f67d
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-10 11:18:04 +0900

    タイムライン投稿取得追加

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 // 新規投稿
 router.post('/', async (req, res) => {
@@ -81,6 +82,16 @@ router.put('/:id/like', async (req, res) => {
 // タイムラインの投稿取得
 router.get('/timeline/all', async (req, res) => {
   try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+
+    return res.status(200).json(userPosts.concat(...friendPosts));
   } catch (err) {
     return res.status(500).json(err);
   }
