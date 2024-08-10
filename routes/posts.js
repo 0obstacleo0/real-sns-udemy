42e8950a1513a771e78f634d484e40a92912436f
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-10 10:56:28 +0900

    Prettier/ESLint導入

--- routes/posts.js
+++ routes/posts.js
@@ -1,81 +1,89 @@
-const router = require("express").Router();
-const Post = require("../models/Post");
+const router = require('express').Router();
+const Post = require('../models/Post');
 
 // 新規投稿
-router.post("/", async (req, res) => {
-    const newPost = new Post(req.body);
-    try {
-        const savedPost = await newPost.save();
-        return res.status(200).json(savedPost);
-    } catch (err) {
-        return res.status(500).json(err);
-    }
+router.post('/', async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 // 投稿編集
-router.put("/:id", async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if (post.userId === req.body.userId) {
-            await Post.updateOne({
-                $set: req.body,
-            });
-            return res.status(200).json("投稿編集に成功しました");
-        } else {
-            return res.status(403).json("他の人の投稿を編集できません");
-        }
-    } catch (err) {
-        return res.json(500).json(err);
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await Post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json('投稿編集に成功しました');
+    } else {
+      return res.status(403).json('他の人の投稿を編集できません');
     }
+  } catch (err) {
+    return res.json(500).json(err);
+  }
 });
 
 // 投稿削除
-router.delete("/:id/delete", async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if (post.userId === req.body.userId) {
-            await post.deleteOne();
-            return res.status(200).json("投稿削除が成功しました");
-        } else {
-            return res.status(403).json("他の人の投稿を削除できません");
-        }
-    } catch (err) {
-        return res.status(500).json(err);
+router.delete('/:id/delete', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      return res.status(200).json('投稿削除が成功しました');
+    } else {
+      return res.status(403).json('他の人の投稿を削除できません');
     }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 // 投稿取得
-router.get("/:id", async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        return res.status(200).json(post);
-    } catch (err) {
-        return res.status(500).json(err);
-    }
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 // 投稿にいいねを押す
-router.put("/:id/like", async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if (!post.likes.includes(req.body.userId)) {
-            await post.updateOne({
-                $push: {
-                    likes: req.body.userId,
-                }
-            });
-            return res.status(200).json("投稿にいいねを押しました");
-        } else {
-            await post.updateOne({
-                $pull: {
-                    likes: req.body.userId,
-                }
-            })
-            return res.status(403).json("投稿からいいねを外しました");
-        }
-    } catch (err) {
-        res.status(500).json(err);
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(200).json('投稿にいいねを押しました');
+    } else {
+      await post.updateOne({
+        $pull: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(403).json('投稿からいいねを外しました');
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// タイムラインの投稿取得
+router.get('/timeline/all', async (req, res) => {
+  try {
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const userRouter = require("./routes/users");
-const authRouter = require("./routes/auth");
-const postRouter = require("./routes/posts");
+const userRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/posts');
 const PORT = 3000;
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_CONN_STR)
-    .then(() => {
-        console.log("DB接続中");
-    }).catch((err) => {
-        console.log(err);
-    })
+mongoose
+  .connect(process.env.MONGODB_CONN_STR)
+  .then(() => {
+    console.log('DB接続中');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/posts", postRouter);
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
 
-app.get("/", (req, res) => {
-    res.send("hello");
-})
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
-app.listen(PORT, console.log("server is running"));
+app.listen(PORT, console.log('server is running'));
